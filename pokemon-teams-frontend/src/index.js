7d77642addb232d15e15b1b9282715d0aad29064
jessesbyers <jesse.s.byers@gmail.com>
2020-02-26 19:24:20 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -5,7 +5,11 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 document.addEventListener("DOMContentLoaded", function() {
     console.log ("DOM Content Loaded")
     fetchTrainers(TRAINERS_URL)
+    console.log("trainers fetched")
+
 })
+
+
 
 function fetchTrainers(url) {
     return fetch(url)
@@ -17,13 +21,12 @@ function fetchTrainers(url) {
     })
 }
 
-
-
 function renderTrainerCard(object) {
     let main = document.querySelector("main")
 
     object.data.map(trainer => {
         let card = document.createElement(`div`)
+        console.log(card)
 
         card.className = "card"
         card.setAttribute("data-id", `${trainer.id}`)
@@ -38,73 +41,71 @@ function renderTrainerCard(object) {
         addPokemonButton.innerText = "Add Pokemon"
         card.appendChild(addPokemonButton)
 
+        resetPokemon(card, trainer)
+        addPokemonButton.addEventListener("click", function(){
+            addPokemon(card, trainer)
+        })
+    })
 
-        let ul = document.createElement(`ul`)
+}
+
+function clearPokemonList(card) {
+        card.querySelector("ul").remove()
+        console.log("ul removed")
+}
+
+function resetPokemon(card, trainer) {
+
+    let ul = document.createElement(`ul`)
+    pokemons = trainer.attributes.pokemons
+
+        pokemons.slice(0, 6).forEach(pokemon => {
+            let li = document.createElement(`li`)
+            li.innerText = `${pokemon.species} (${pokemon.nickname})`
+            ul.appendChild(li)
+            let button = document.createElement(`button`)
+            button.setAttribute("data-pokemon-id", `${pokemon.id}`)
+            button.className = "release"
+            button.innerText = "Release"
+            li.appendChild(button)
+        })
         card.appendChild(ul)
-        
-        resetPokemon(ul, trainer)
-        addPokemon(ul, trainer, addPokemonButton)
-    })
-    // addPokemon(ul, trainer, addPokemonButton)
-
 }
 
-function resetPokemon(ul, trainer) {
-    let pokemons = trainer.attributes.pokemons
-// need to clear out li's so that I don't get repeats
-    console.log(ul)
 
-    pokemons.forEach(pokemon => {
-        let li = document.createElement(`li`)
-        li.innerText = `${pokemon.species} (${pokemon.nickname})`
-        ul.appendChild(li)
-        let button = document.createElement(`button`)
-        button.setAttribute("data-pokemon-id", `${pokemon.id}`)
-        button.className = "release"
-        button.innerText = "Release"
-        li.appendChild(button)
-
-        removePokemon(pokemon, button)
-    })
-}
-
-// set event listener for add pokemon
-function addPokemon(ul, trainer, button) {
-    button.addEventListener("click", function(){
-
-        let configObj = {
-            method: "POST",
-            headers: 
-            {
-              "Content-Type": "application/json",
-              "Accept": "application/json"
-            },
-            body: JSON.stringify({
-              trainer_id: trainer.id
-            })
-        };
-          
-        return fetch(POKEMONS_URL, configObj)
-        .then(function(response) {
-            response.json();
+function addPokemon(card, trainer) {
+    let configObj = {
+        method: "POST",
+        headers: 
+        {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({
+        trainer_id: trainer.id
         })
-        .then(function(json) {
-        resetPokemon(ul, trainer)
+    };
+    
+
+// something wrong with this syntax...
+    return fetch(POKEMONS_URL, configObj)
+        .then(response => response.json())
+        .then(json =>  {
         console.log("pokemon added")
-        })
+        clearPokemonList(card)
+        resetPokemon()
     })
 }
+
 
 
 // set event listener for remove pokemon
 function removePokemon(pokemon, button) {
     button.addEventListener("click", function(){
 
-    console.log("pokemon removed")
-    console.log(button)
-    console.log(pokemon)
-
-
+    // console.log("pokemon removed")
+    // console.log(button)
+    // console.log(pokemon)
 
     // remove this pokemon from the trainer's relationship database and from the view
     })
